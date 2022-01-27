1ab77e6a6b8aab879360f281bafc41857e829c08
Mayank <gr7mayankbansal@gmail.com>
2022-01-27 11:48:28 +0530

    TextUtils App Done With All Basics

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import About from './Components/About';
+import Alert from './Components/Alert';
+import Navbar from './Components/Navbar';
+import TextForm from './Components/TextForm';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
+  const toToggle = () =>{
+    if(mode === 'light'){
+      setMode('dark');
+      document.body.style.backgroundColor = '#171616';
+      document.body.style.color = '#d3dbe4';
+      showAlert('Dark Mode-On', 'secondary');
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor = 'rgb(240 237 237)';
+      document.body.style.color = 'rgb(7 54 107)';
+      showAlert('Light Mode-On', 'primary');
+    }
+  }
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+  const showAlert = (message, type) =>{
+    setAlert({
+      msg: message,
+      type: type
+    })
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    {/* <div className="container my-5">
+      <TextForm heading='Enter Your Text Here:' mode={mode} showAlert={showAlert}/>
+    </div> */}
+    {/* <Navbar/> */}
+    <Router>
+    <Navbar title='TextUtils' mode={mode} toggle={toToggle}/>
+    <Alert alert={alert}/>
+      <div className="container my-5">
+      <Routes>
+          <Route path="/about" element={<About />} />
+          <Route path="/" element={<TextForm heading='Enter Your Text Here:' mode={mode} showAlert={showAlert}/>} />
+      </Routes>
+      </div>
+    </Router>
+    </>
   );
 }
 
---
+++ src/Components/TextForm.js
@@ -0,0 +1,91 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types'
+
+export default function TextForm(props) {
+    const toStyle_d = {
+         border : "5px solid #c7bebe",
+         backgroundColor : '#1f1d1d',
+         fontWeight : "bold",
+         color : "white",
+         overflow: 'hidden'
+     }
+    const toStyle_l = {
+        border : "5px solid black",
+        backgroundColor : 'white',
+        fontWeight : "bold",
+        color : "black",
+        overflow: 'hidden'
+    }
+    let word = {
+       ct_word : () =>{
+           if(text.length <= 0){
+               return 0;
+           }
+           else{
+               return text.split(' ').length;
+           }
+       }
+   }
+    const toClear = ()=>{
+        setText('');
+        if(text.length > 0) props.showAlert('Cleared Successfully', 'success');
+    }
+    const toCopyText = ()=>{
+        let copy_text = document.getElementById('myBox');
+        copy_text.select();
+        navigator.clipboard.writeText(copy_text.value);
+        if(text.length > 0) props.showAlert('Copied Successfully', 'success');
+    }
+    const toUpperCase = ()=>{
+        let updatedText = text.toUpperCase();
+        setText(updatedText);
+        if(text.length > 0) props.showAlert('Converted To Upper Case Successfully', 'success');
+    }
+
+    const toRemoveExtraSpace = ()=>{
+        let removeExtraSpace = text.split(/[ ]+/);
+        setText(removeExtraSpace.join(" "));
+        if(text.length > 0) props.showAlert('Extra Space Removed Successfully', 'success');
+    }
+    const toLowerCase = ()=>{
+        let updatedText = text.toLowerCase();
+        setText(updatedText);
+        if(text.length > 0) props.showAlert('Converted To Lower Case Successfully', 'success');
+    }
+    const onHandleChange = (event)=>{
+        setText(event.target.value);
+    }
+    const [text, setText] = useState('');
+  return ( 
+    <>
+    <div>
+        <h5>{props.heading}</h5>
+        <div className="mb-3">
+        <textarea className="form-control" onChange={onHandleChange} value={text} id="myBox" rows="7" style={props.mode === 'light' ? toStyle_l : toStyle_d} spellCheck='false'></textarea>
+        </div>
+        <button type="button" onClick={toClear} className="btn btn-danger my-3 mx-2">Clear</button>
+        <button type="button" onClick={toCopyText} className="btn btn-success my-3 mx-2">Copy</button>
+        <button type="button" onClick={toUpperCase} className={`btn btn-${props.mode=== 'light' ? 'primary' : 'secondary'} my-3 mx-2`}>Upper Case</button>
+        <button type="button" onClick={toLowerCase} className={`btn btn-${props.mode=== 'light' ? 'primary' : 'secondary'} my-3 mx-2`}>Lower Case</button>
+        <button type="button" onClick={toRemoveExtraSpace} className={`btn btn-${props.mode=== 'light' ? 'primary' : 'secondary'} my-3 mx-2`}>Remove Extra Space</button>
+    </div>
+    <div className="container">
+        <h4>Your Text Summary</h4>
+        <p>Word: {word.ct_word()} Character: {text.length - text.split(' ').length + 1}</p>
+        <p>Minutes Read: {0.008 * word.ct_word()}</p>
+    </div>
+    <div className="container">
+        <h5>Preview</h5>
+        <p>{text.length > 0 ? text : 'Enter some text above to preview it here...'}</p>
+    </div>
+    </> 
+  )
+}
+
+TextForm.propTypes = {
+    heading: PropTypes.string
+}
+
+TextForm.defaultProps = {
+    heading: 'Enter Text'
+}
