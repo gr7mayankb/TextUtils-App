d5e8726c8362f49af686c1d05f8d7e1e640613d6
Mayank <gr7mayankbansal@gmail.com>
2022-01-27 12:16:46 +0530

    Bug fix

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -6,7 +6,7 @@ export default function Navbar(props) {
   return (
     <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
     <div className="container-fluid">
-      <h1 className="navbar-brand mx-1 font-weight-bolder">{props.title}</h1>
+      <Link className="navbar-brand mx-1 font-weight-bolder" to="/">{props.title}</Link>
       <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
